4fcf3f59019b8f2669997560bffc064d5aa44335
lubson <l.hruban@gmail.com>
2012-02-15 16:45:46 +0100

    App generated

---
+++ app/controllers/Publication.java
@@ -0,0 +1,18 @@
+
+package controllers;
+
+import play.*;
+
+import play.mvc.*;
+
+import java.util.*;
+
+import models.*;
+
+//@With(Secure.class)
+public class Publication extends Controller {
+    public static void index() {
+        render();
+    }
+
+}
