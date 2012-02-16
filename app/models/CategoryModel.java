a3da59d6e60f5e87acec7a79e7637c365427f56f
lubson <l.hruban@gmail.com>
2012-02-16 15:52:24 +0100

    One to many depenedency added

--- app/models/CategoryModel.java
+++ app/models/CategoryModel.java
@@ -3,6 +3,9 @@ package models;
 import play.*;
 import javax.persistence.*;
 
+import java.util.List;
+import java.util.ArrayList;
+
 import play.db.jpa.*;
 
 @Entity
@@ -10,7 +13,11 @@ public class CategoryModel extends Model {
   
   public String name;
   
+  @OneToMany(mappedBy="categoryModel", cascade=CascadeType.ALL)
+  public List<TypeModel> typeModel;
+  
   public CategoryModel(String name) {
+    this.typeModel = new ArrayList<TypeModel>();
     this.name = name;
   }
 }
